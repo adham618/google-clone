e739fa6e0daae1e61d19555696a1fdb93a9bd10d
adham618 <adhamtarek291@gmail.com>
2021-12-30 20:56:12 +0200

    starter project with  tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Google</title>
+        <meta name="description" content="Google Clone" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">
+      </main>
+    </div>
+  )
+}
+
+export default Home
