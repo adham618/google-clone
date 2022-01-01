84fdd2bce08121155882ac6b24aa4c29ce1d1dc3
adham618 <adhamtarek291@gmail.com>
2022-01-01 22:26:35 +0200

    make search function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { MicrophoneIcon, SearchIcon, ViewGridIcon } from '@heroicons/react/solid'
-
 import Avater from '../common/components/Avater/Avater'
 import Image from 'next/image'
 import Footer from '../common/components/Footer/Footer'
+import { useRouter } from 'next/router'
+import { MutableRefObject, useRef } from 'react'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+
+  const searchInputRef = useRef(null) as unknown as MutableRefObject<HTMLInputElement>
+
+  const search = (e: { preventDefault: () => void }) => {
+    e.preventDefault()
+    const term = searchInputRef.current.value
+
+    if (!term) return
+    router.push(`/search?term=${term}`)
+  }
+
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
       <Head>
@@ -43,12 +56,12 @@ const Home: NextPage = () => {
         />
         <div className='flex w-full mt-5 hover:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
           <SearchIcon className='h-5 mr-3 text-gray-500' />
-          <input type="text" name="" id="" className='flex-grow focus:outline-none' />
+          <input ref={searchInputRef} type="text" name="" id="" className='flex-grow focus:outline-none' />
           <MicrophoneIcon className='h-5' />
         </div>
         <div className='flex flex-col w-1/2 space-y-2 justify-center mt-6 sm:space-y-0 sm:flex-row sm:space-x-4'>
-          <button className='btn'>Google Search</button>
-          <button className='btn'>I&apos;m Feeling  Lucky</button>
+          <button onClick={search} className='btn'>Google Search</button>
+          <button onClick={search} className='btn'>I&apos;m Feeling  Lucky</button>
         </div>
       </form>
       {/* Footer */}
