ee16f7f9fe4a15f6b47bde9ec641dcd3e5209501
adham618 <adhamtarek291@gmail.com>
2022-01-01 21:10:41 +0200

    make the Search Form and add Footer Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Avater from '../common/components/Avater'
+import { MicrophoneIcon, SearchIcon, ViewGridIcon } from '@heroicons/react/solid'
+
+import Avater from '../common/components/Avater/Avater'
+import Image from 'next/image'
+import Footer from '../common/components/Footer/Footer'
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className='flex flex-col justify-center items-center h-screen'>
       <Head>
         <title>Google</title>
         <meta name="description" content="Google Clone" />
@@ -14,20 +18,42 @@ const Home: NextPage = () => {
       {/* Header */}
       <header className="flex w-full p-5 justify-between text-sm text-gray-700">
         {/* Left */}
-        <div className="flex space-x-4 items-center"></div>
+        <div className="flex space-x-4 items-center">
+          <p className='link'>About</p>
+          <p className='link'>Store</p>
+        </div>
         {/* Right */}
         <div className="flex space-x-4 items-center ">
           <p className='link'>Gmail</p>
           <p className='link'>Images</p>
           {/* Icon */}
+          <ViewGridIcon className='h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer' />
           {/* Avater */}
           <Avater url={'/photo.jpg'} />
         </div>
       </header>
       {/* Body */}
+      <form className='flex flex-col items-center mt-5 flex-grow mx-5 w-4/5'>
+        <Image
+          src={"https://www.google.com/logos/doodles/2022/new-years-day-2022-6753651837109338-law.gif"}
+          width={500}
+          height={200}
+          alt="google-image"
+          draggable="false"
+        />
+        <div className='flex w-full mt-5 hover:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl'>
+          <SearchIcon className='h-5 mr-3 text-gray-500' />
+          <input type="text" name="" id="" className='flex-grow focus:outline-none' />
+          <MicrophoneIcon className='h-5' />
+        </div>
+        <div className='flex flex-col w-1/2 space-y-2 justify-center mt-6 sm:space-y-0 sm:flex-row sm:space-x-4'>
+          <button className='btn'>Google Search</button>
+          <button className='btn'>I&apos;m Feeling  Lucky</button>
+        </div>
+      </form>
       {/* Footer */}
-
-    </>
+      <Footer />
+    </div>
   )
 }
 
