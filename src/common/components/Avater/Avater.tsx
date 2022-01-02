afe963b1f495c6012cf8769024d2a2cc0f6a6bbb
adham618 <adhamtarek291@gmail.com>
2022-01-02 17:43:33 +0200

    add className props

--- src/common/components/Avater/Avater.tsx
+++ src/common/components/Avater/Avater.tsx
@@ -1,13 +1,21 @@
 import Image from "next/image"
 
 interface AvaterProps {
-  url: string
+  url: string,
+  className: string
 }
 
-const Avater = ({ url }: AvaterProps) => {
+const Avater = ({ url, className }: AvaterProps) => {
   return (
     // eslint-disable-next-line @next/next/no-img-element
-    <img className="h-10 w-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 hover:rounded-[50%]" src={url} width={35} height={35} alt="profile-pic" draggable="false" />
+    <img
+      className={`h-10 w-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 hover:rounded-[50%]" ${className} `}
+      src={url}
+      width={35}
+      height={35}
+      alt="profile-pic"
+      draggable="false"
+    />
   )
 }
 export default Avater
