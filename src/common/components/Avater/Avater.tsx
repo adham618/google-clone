e216c4b9aec7cad8ae6998eb1d3afcc1b5be072c
adham618 <adhamtarek291@gmail.com>
2022-01-02 19:19:44 +0200

    adjust width && height Avater

--- src/common/components/Avater/Avater.tsx
+++ src/common/components/Avater/Avater.tsx
@@ -12,7 +12,7 @@ const Avater = ({ url, className }: AvaterProps) => {
       className={`h-10 w-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 hover:rounded-[50%]" ${className} `}
       src={url}
       width={35}
-      height={35}
+      height={40}
       alt="profile-pic"
       draggable="false"
     />
